7a678f644e3ae7bdbb4a8e9a13b65f0ebb52b332
Germán Heim <germanheim@hotmail.com>
2025-10-04 10:22:02 -0300

    Remove build script and update Cargo.toml
    Deleted build.rs and removed its reference from Cargo.toml.  This is because the warning is on the website now, and most people already know about IO/Thread management.
    
    Added docs.rs metadata for documentation configuration.

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main() {
-    if env::var("CARGO_FEATURE_RAYON").is_ok() {
-        println!(
-            "cargo:warning=[Performance Notice] The 'rayon' feature is enabled. In debug builds or low workload scenarios, the overhead of thread management might reduce performance. For production builds, consider benchmarking with and without 'rayon'."
-        );
-    }
-}
